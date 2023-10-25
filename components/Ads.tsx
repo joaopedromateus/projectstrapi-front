252c0c59d57dec2e82a9141e60d32cfdfff480f6
joao.mateus <joao.mateus@softplan.com.br>
2023-10-25 17:13:26 -0300

    first commit

---
+++ components/Ads.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Ads = () => {
+  return(
+    <div className="container mx-auto xl:max-w-[1180px] flex items-center justify-center pt-[35px]"> 
+      <img src="/assets/ads.png" alt="" className="w-[80%] cursor-pointer h-[100px] w-[750px]" />
+      
+    </div>
+  )
+}
+
+export default Ads
